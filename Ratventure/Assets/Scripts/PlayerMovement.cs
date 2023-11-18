96e7b37c0947c196a48cce1e7da901b02643986d
Zittinho <titanzittoyt2@mail.ru>
2023-11-18 19:18:40 +1000

    Fixed wall collision + New Camera

--- Ratventure/Assets/Scripts/PlayerMovement.cs
+++ Ratventure/Assets/Scripts/PlayerMovement.cs
@@ -1,11 +1,10 @@
 using UnityEngine;
 
-
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] private float speed; // скарость персонажа
+    [SerializeField] private float speed; // скорость персонажа
     [SerializeField] public float jumpHeight = 5; // начальная сила прыжка
-    [SerializeField] public float gravityScale = 5; // сила гравитация
+    [SerializeField] public float gravityScale = 5; // сила гравитации
     [SerializeField] private LayerMask groundLayer;
     [SerializeField] private LayerMask wallLayer;
     private Rigidbody2D body;
@@ -15,7 +14,6 @@ public class PlayerMovement : MonoBehaviour
     float jumpTime;
     bool jumping;
     bool jumpCancelled;
-
 
     // Start is called before the first frame update
     private void Start()
@@ -29,10 +27,18 @@ public class PlayerMovement : MonoBehaviour
     {
         // движение вперед и назад
         float dirX = Input.GetAxis("Horizontal");
-        
-        body.velocity = new Vector2(dirX * speed, body.velocity.y);
 
-        //компонент прыжка 
+        // Проверка на касание стены перед выполнением движения
+        if (dirX > 0 && !CheckWallCollision(Vector2.right))
+        {
+            body.velocity = new Vector2(dirX * speed, body.velocity.y);
+        }
+        else if (dirX < 0 && !CheckWallCollision(Vector2.left))
+        {
+            body.velocity = new Vector2(dirX * speed, body.velocity.y);
+        }
+
+        // компонент прыжка 
         if (Input.GetButtonDown("Jump") && isGrounded())
         {
             float jumpForce = Mathf.Sqrt(jumpHeight * -2 * (Physics2D.gravity.y * body.gravityScale));
@@ -54,6 +60,7 @@ public class PlayerMovement : MonoBehaviour
             }
         }
     }
+
     private void FixedUpdate()
     {
         if (jumpCancelled && jumping && body.velocity.y > 0)
@@ -62,13 +69,23 @@ public class PlayerMovement : MonoBehaviour
         }
     }
 
-    // функция для отслеживания персонажа на земле ли он
+    // функция для отслеживания персонажа на земле
     private bool isGrounded()
     {
         RaycastHit2D raycastHit = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0, Vector2.down, 0.1f, groundLayer);
         return raycastHit.collider != null;
     }
 
-}
+    // функция для проверки касания стены
+    private bool CheckWallCollision(Vector2 direction)
+    {
+        float extraWidth = 0.05f;
+        float extraHeight = 0.1f; // добавим небольшой запас высоты для точного обнаружения стены
 
-    
+        // Используем Raycast с учетом дополнительной высоты для точного обнаружения стены
+        RaycastHit2D hit = Physics2D.Raycast(boxCollider.bounds.center, direction, boxCollider.bounds.extents.x + extraWidth, wallLayer);
+
+        // Проверяем, находится ли персонаж на земле и не произошло ли касание стены во время прыжка
+        return hit.collider != null && (!jumping || hit.collider.CompareTag("Ground"));
+    }
+}
