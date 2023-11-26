0dd78d29ed92d7ead59ec12f66ea71d1f4fd9fd7
Zittinho <titanzittoyt2@mail.ru>
2023-11-26 21:05:30 +1000

    CheckPoint and Reload Scene

--- Ratventure/Assets/Scripts/PlayerMovement.cs
+++ Ratventure/Assets/Scripts/PlayerMovement.cs
@@ -1,45 +1,48 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] private float speed; // Скорость передвижения персонажа
-    [SerializeField] public float jumpHeight = 5; // Высота прыжка персонажа
-    [SerializeField] public float gravityScale = 5; // Значение силы гравитации
-    [SerializeField] private LayerMask groundLayer; // Слой для определения поверхности земли
-    [SerializeField] private LayerMask wallLayer; // Слой для определения стен
+    [SerializeField] private float speed;
+    [SerializeField] public float jumpHeight = 5;
+    [SerializeField] public float gravityScale = 5;
+    [SerializeField] private LayerMask groundLayer;
+    [SerializeField] private LayerMask wallLayer;
     private Rigidbody2D body;
     private BoxCollider2D boxCollider;
     private SpriteRenderer sprite;
     private Animator animator;
-    
+
     private float dirX = 0f;
-    public float buttonTime = 0.2f; // Время удержания кнопки прыжка
-    public float cancelRate = 100; // Скорость отмены прыжка
-    
-    private enum MovementState {standing, running, jumping, falling }
+    public float buttonTime = 0.2f;
+    public float cancelRate = 100;
+
+    private enum MovementState { standing, running, jumping, falling }
 
     float jumpTime;
     bool jumping;
     bool jumpCancelled;
 
-    // Вызывается перед первым кадром
+    private Vector2 initialPosition = new Vector2(1, 1.16f);
+    //private Vector2 checkpointPosition;
+    private const string CheckpointXKey = "CheckpointX";
+    private const string CheckpointYKey = "CheckpointY";
     private void Start()
     {
         body = GetComponent<Rigidbody2D>();
         boxCollider = GetComponent<BoxCollider2D>();
         sprite = GetComponent<SpriteRenderer>();
         animator = GetComponent<Animator>();
+
+        LoadCheckpointPosition();
     }
 
-    // Вызывается один раз за кадр
     private void Update()
     {
-        // Управление движением влево и вправо
         dirX = Input.GetAxis("Horizontal");
 
         if (Input.GetButton("Horizontal"))
         {
-            // Проверка наличия стены перед движением влево или вправо
             if (dirX > 0 && !CheckWallCollision(Vector2.right))
             {
                 body.velocity = new Vector2(dirX * speed, body.velocity.y);
@@ -48,19 +51,8 @@ public class PlayerMovement : MonoBehaviour
             {
                 body.velocity = new Vector2(dirX * speed, body.velocity.y);
             }
-
         }
 
-        //   if (is_flipix && !sprite.flipX || !is_flipix && sprite.flipX) // если произошёл поворт персонажа, то работа с коллайдером
-        //   {
-        //        Vector2 colliderSize = boxCollider.size; // Получить текущие размеры коллайдера
-        //                                                // colliderSize.y *= -1; // Отразить размеры коллайдера по вертикали
-        //        boxCollider.size = colliderSize; // Применить новые размеры к коллайдеру
-        //   }
-        
-            
-
-        // Управление прыжком
         if (Input.GetButtonDown("Jump") && isGrounded())
         {
             float jumpForce = Mathf.Sqrt(jumpHeight * -2 * (Physics2D.gravity.y * body.gravityScale));
@@ -70,7 +62,6 @@ public class PlayerMovement : MonoBehaviour
             jumpTime = 0;
         }
 
-        // Логика управления продолжительностью прыжка
         if (jumping)
         {
             jumpTime += Time.deltaTime;
@@ -84,32 +75,38 @@ public class PlayerMovement : MonoBehaviour
             }
         }
 
-        // функция анимации
         UpdateAnimationState();
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            ResetCheckpoint();
+        }
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            ReloadScene();
+        }
     }
 
-    private void UpdateAnimationState() //Состояние анимации обновления 
+    private void UpdateAnimationState()
     {
         MovementState state;
 
-        // Анимация бега
         if (dirX > 0f)
         {
             state = MovementState.running;
-            sprite.flipX = false; //Разворот игрока
-        //    sprite.flip
+            sprite.flipX = false;
         }
         else if (dirX < 0f)
         {
             state = MovementState.running;
-            sprite.flipX = true; //Разворот игрока
+            sprite.flipX = true;
         }
         else
         {
             state = MovementState.standing;
         }
 
-        // Проверка анимации прыжков 
         if (body.velocity.y > .1f)
         {
             state = MovementState.jumping;
@@ -122,33 +119,66 @@ public class PlayerMovement : MonoBehaviour
         animator.SetInteger("state", (int)state);
     }
 
-    // Вызывается фиксированное количество раз в секунду
     private void FixedUpdate()
     {
-        // Отмена прыжка, если кнопка была отпущена во время прыжка вверх
         if (jumpCancelled && jumping && body.velocity.y > 0)
         {
             body.AddForce(Vector2.down * cancelRate);
         }
     }
 
-    // Проверка, находится ли персонаж на земле
     private bool isGrounded()
     {
         RaycastHit2D raycastHit = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0, Vector2.down, 0.1f, groundLayer);
         return raycastHit.collider != null;
     }
 
-    // Проверка наличия стены
     private bool CheckWallCollision(Vector2 direction)
     {
         float extraWidth = 0.05f;
-        //float extraHeight = 0.1f; // Добавим запас высоты для точного обнаружения стены
-
-        // Используем Raycast с учетом дополнительной высоты для точного обнаружения стены
         RaycastHit2D hit = Physics2D.Raycast(boxCollider.bounds.center, direction, boxCollider.bounds.extents.x + extraWidth, wallLayer);
-
-        // Проверяем, находится ли персонаж на земле и не произошло ли касание стены во время прыжка
         return hit.collider != null && (!jumping || hit.collider.CompareTag("Ground"));
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("CheckPoint"))
+        {
+
+            PlayerPrefs.SetFloat("CheckpointX", transform.position.x);
+            PlayerPrefs.SetFloat("CheckpointY", transform.position.y);
+            PlayerPrefs.Save();
+
+            Debug.Log("Checkpoint saved!");
+        }
+    }
+
+    private void ReloadScene()
+    {
+        // Перезагружаем текущую сцену
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    private void LoadCheckpointPosition()
+    {
+        // Загружаем позицию чекпоинта из PlayerPrefs
+        float checkpointX = PlayerPrefs.GetFloat("CheckpointX", initialPosition.x);
+        float checkpointY = PlayerPrefs.GetFloat("CheckpointY", initialPosition.y);
+
+        // Устанавливаем игрока на позицию чекпоинта
+        transform.position = new Vector2(checkpointX, checkpointY);
+    }
+
+
+    private void ResetCheckpoint()
+    {
+        // Устанавливаем игрока на изначальные координаты
+        transform.position = initialPosition;
+
+        // Сбрасываем сохраненные позиции чекпоинта
+        PlayerPrefs.DeleteKey("CheckpointX");
+        PlayerPrefs.DeleteKey("CheckpointY");
+
+        Debug.Log("Checkpoint reset!");
     }
 }
